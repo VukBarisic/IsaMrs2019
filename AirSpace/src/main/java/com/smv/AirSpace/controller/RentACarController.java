fafc5c343f6389bf7701ca4fe775ba760b5ecc2f
zrnke <zrnkemiki@gmail.com>
2019-03-27 23:58:29 +0100

    Pretraga vozila

--- AirSpace/src/main/java/com/smv/AirSpace/controller/RentACarController.java
+++ AirSpace/src/main/java/com/smv/AirSpace/controller/RentACarController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -43,8 +44,20 @@ public class RentACarController {
 			// TODO: handle exception
 			return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
+		
+		
 	}
 	
-	
+	@RequestMapping(value="/searchVehicles/{param}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Vehicle>> searchVehicles(@PathVariable String param){
+		try {
+			System.out.println("pretraga");
+			System.out.println(param);
+			return new ResponseEntity<List<Vehicle>>(rentACarService.findByModel(param), HttpStatus.OK);
+		} catch (Exception e) {
+			// TODO: handle exception
+			return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 	
 }
--- AirSpace/src/main/java/com/smv/AirSpace/service/RentACarService.java
+++ AirSpace/src/main/java/com/smv/AirSpace/service/RentACarService.java
@@ -11,7 +11,7 @@ import com.smv.model.Vehicle;
 @Service
 public class RentACarService {
 	
-	public static List<Vehicle> vehicles = new ArrayList<Vehicle>();
+	public static List<Vehicle> vehicles;
 	
 	public void addVehicle(VehicleDTO vehicleDTO) {
 		Vehicle vehicle = new Vehicle(0L, vehicleDTO.getModel(), vehicleDTO.getNumOfSeats());
@@ -20,13 +20,28 @@ public class RentACarService {
 	}
 	
 	public List<Vehicle> getAllVehicles(){
+		vehicles = new ArrayList<Vehicle>();
 		Vehicle vehicle1 = new Vehicle(1L, "Opel", 5);
 		Vehicle vehicle2 = new Vehicle(2L, "Mercedes", 5);
 		Vehicle vehicle3 = new Vehicle(3L, "BMW", 4);
 		vehicles.add(vehicle1);
 		vehicles.add(vehicle2);
 		vehicles.add(vehicle3);
+		System.out.println("Dodata dummy vozila");
 		
 		return vehicles;
+	}
+	
+	public List<Vehicle> findByModel(String param) {
+		List<Vehicle> results = new ArrayList<Vehicle>();
+		for (Vehicle vehicle : vehicles) {
+			if (vehicle.getModel().toLowerCase().contains(param.toLowerCase())){
+				results.add(vehicle);	
+			}
+			else
+				continue;
+		}
+		return results;
+		
 	}
 }
